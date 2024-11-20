1d98854745ff2c6a44131b615d001c5f9fc82db6
Ehte22 <syedehteshamali22@gmail.com>
2024-11-20 22:30:21 +0530

    number updated

--- server/api/index.js
+++ server/api/index.js
@@ -3,13 +3,13 @@ const express = require("express")
 const cookieParser = require("cookie-parser")
 const cors = require("cors")
 const path = require("path")
-const { userProtected } = require("./middleware/userProtected")
-require("dotenv").config({ path: "./.env" })
+const { userProtected } = require("../middleware/userProtected")
+require("dotenv").config({ path: "./api/.env" })
 
 mongoose.connect(process.env.MONGO_URL)
 
 const app = express()
-app.use(express.static(path.join(__dirname, "dist")))
+// app.use(express.static(path.join(__dirname, "dist")))
 app.use(express.static("users"))
 app.use(express.static("dishes"))
 app.use(express.json())
@@ -28,16 +28,16 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 
-app.use("/api/auth", require("./routes/authRoute"))
-app.use("/api/admin", require("./routes/dishRoute"))
-app.use("/api/user", userProtected, require("./routes/userRoute"))
+app.use("/api/auth", require("../routes/authRoute"))
+app.use("/api/admin", require("../routes/dishRoute"))
+app.use("/api/user", userProtected, require("../routes/userRoute"))
 
 
 
 app.use("*", (req, res) => {
     console.log('Wildcard');
-    res.sendFile(path.join(__dirname, "dist", "index.html"))
-    // res.status(404).json({ message: "No resource found" })
+    // res.sendFile(path.join(__dirname, "dist", "index.html"))
+    res.status(404).json({ message: "No resource found" })
 })
 
 app.use((err, req, res, next) => {
