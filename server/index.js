fef4cc78084c09ceeb8b39796f2ea15ffb55fa4d
Ehte22 <syedehteshamali22@gmail.com>
2024-11-26 14:20:24 +0530

    vercel updated

--- server/index.js
+++ server/index.js
@@ -16,7 +16,6 @@ app.use(express.json())
 app.use(cookieParser())
 
 const corsOptions = {
-    // origin: "http://localhost:5173",
     origin: "https://food-master-ten.vercel.app",
     credentials: true
 };
